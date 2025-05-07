4e2e69f8b87a95f3261a8147643c46ca27d9c5bc
cryptorexx <takealiftoff@gmail.com>
2025-05-07 16:34:34 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
 
